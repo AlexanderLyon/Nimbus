c2b54bd7fc59fe0907536ceb1aba6c44e2296141
Alexander Lyon <alyon011@gmail.com>
2019-01-20 16:47:51 -0500

    Add fixed header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,16 +10,14 @@ export class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1 className='title'>Weather Forecast</h1>
-          <p id="location" onClick={this.props.openLocationMenu}>
-            <i className="fas fa-location-arrow"></i> {this.props.cityName}
-          </p>
-          <p id="last-updated">Last updated: {this.props.lastUpdated}</p>
-          <hr/>
-          <button id="refreshBtn" className="btn"
-            onClick={this.props.fetchData}>
-              <i className={ this.props.refreshing ? "refreshing fas fa-sync-alt" : "fas fa-sync-alt" }></i>Refresh
-          </button>
+        <p id="location" onClick={this.props.openLocationMenu}>
+          <i className="fas fa-location-arrow"></i> {this.props.cityName}
+        </p>
+        <p id="last-updated"><i class="fas fa-clock"></i> Last updated: {this.props.lastUpdated}</p>
+        <button id="refreshBtn" className="btn"
+          onClick={this.props.fetchData}>
+            <i className={ this.props.refreshing ? "refreshing fas fa-sync-alt" : "fas fa-sync-alt" }></i>Refresh
+        </button>
       </header>
     );
   }
